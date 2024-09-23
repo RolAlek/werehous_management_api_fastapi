42b6445c8c9c4cff5df899f71005bd4651bc596f
Aleksandr Rol <rolaleksandr75@gmail.com>
2024-09-23 22:39:28 +0900

    include main router to app

--- fastpi-app/main.py
+++ fastpi-app/main.py
@@ -1,7 +1,11 @@
 import uvicorn
 from fastapi import FastAPI
 
+from api import main_router
+from core.config import settings
+
 main_app = FastAPI()
+main_app.include_router(main_router, prefix=settings.api.prefix)
 
 if __name__ == '__main__':
     uvicorn.run('main:main_app', reload=True)
